555272248b3ef5de8a5a5aedbdab1347a478f697
maRia-1506A <zannatulfarzana363@gmail.com>
2025-05-29 00:03:52 +0600

    new file

---
+++ Snake_Water_GunGame.py
@@ -0,0 +1,40 @@
+'''
+1 for snake
+-1 for water
+0 for gun
+'''
+import random
+computer = random.choice([1, -1, 0])  # variable 1
+user = input("Enter your choice: ")
+userChoice = {
+    "s": 1,
+    "w": -1,
+    "g": 0
+}
+mainUser = userChoice[user]  # variable 2
+reverseUserCoice = {
+    1: "Snake",
+    -1: "Water",
+    0: "Gun"
+}  # to print the choices
+
+print(
+    f"You chose {reverseUserCoice[mainUser]}\nComputer chose {reverseUserCoice[computer]}")
+
+if (computer == mainUser):
+    print("Game Draw!")
+else:
+    if (computer == 1 and mainUser == -1):
+        print("You Lose!")
+    elif (computer == 1 and mainUser == 0):
+        print("You Win!")
+    elif (computer == -1 and mainUser == 1):
+        print("You win!")
+    elif (computer == -1 and mainUser == 0):
+        print("You Lose!")
+    elif (computer == 0 and mainUser == -1):
+        print("You Win!")
+    elif (computer == 0 and mainUser == 1):
+        print("You Lose!")
+    else:
+        print("Something went wrong!")
