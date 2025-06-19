0fad2bfa2661383d260e9c491cdb59585da34c63
maRia-1506A <zannatulfarzana363@gmail.com>
2025-06-19 21:20:30 +0600

    Porject

--- NumberGuess.py
+++ NumberGuess.py
@@ -1,13 +1,29 @@
-import random
-RandomNumb = random.randrange(1, 200)
-UserInput = int(input("Enter the number: "))
+# import random
+# RandomNumb = random.randrange(1, 200)
+# UserInput = int(input("Enter the number: "))
 
-if UserInput > RandomNumb:
-    print(RandomNumb)
-    print("The number is too high")
-elif UserInput < RandomNumb:
-    print(RandomNumb)
-    print("The numb is too low")
-else:
-    print(RandomNumb)
-    print("Congratulations! You matched the number")
+# if UserInput > RandomNumb:
+#     print(RandomNumb)
+#     print("The number is too high")
+# elif UserInput < RandomNumb:
+#     print(RandomNumb)
+#     print("The numb is too low")
+# else:
+#     print(RandomNumb)
+#     print("Congratulations! You matched the number")
+
+
+import random
+randomNumb = random.randint(1, 100)
+guess = 0
+a = -1  # Initialize with -1 before using it in while loop
+
+while (a != randomNumb):
+    guess += 1
+    a = int(input("Guess anumber: "))
+    if (a > randomNumb):
+        print("Lower number please!")
+    else:
+        print("Higher Number please!")
+
+print(f"You have guessed the correct number {randomNumb} in {guess} guesses")
