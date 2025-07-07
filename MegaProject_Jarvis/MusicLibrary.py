34e8b138e0e19bd72b1630006cb7a6e21d1711e3
maRia-1506A <zannatulfarzana363@gmail.com>
2025-07-08 02:31:16 +0600

    half project

---
+++ MegaProject_Jarvis/MusicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "gulbahar": "https://www.youtube.com/watch?v=Ib_L3vuUX5k&list=RDIb_L3vuUX5k&start_radio=1&pp=oAcB",
+    "perfect": "https://youtu.be/fTrqoVSrw1Y?si=8e9dPvuBm7nFMJBc",
+    "jhol": "https://youtu.be/-2RAq5o5pwc?si=i3kP_i2UGFUSrlZV",
+    "bulbuli": "https://youtu.be/9c1NIrAqXe4?si=g1OQH17Mb0Mjr5hp",
+    "baby": "https://youtu.be/LTsR3LfhdxI?si=vxMbsP8eisuHDEA4"
+
+}
