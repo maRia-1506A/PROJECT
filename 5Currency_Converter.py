28175291a43c136c5809ee64b45a8fe455745e98
maRia-1506A <zannatulfarzana363@gmail.com>
2025-07-22 22:16:44 +0600

    half done project

--- 5Currency_Converter.py
+++ 5Currency_Converter.py
@@ -1,7 +1,18 @@
-from currency_converter import CurrencyConverter
-c = CurrencyConverter()
+with open("5Currency_Converter.txt") as f:
+    lines = f.readlines()
 
-ask = int(input("Enter the amount: "))
-source = input("Source currency (USD/EUR/CAD): ")
-target = input("Target currency (USD/EUR/CAD): ")
+currencyDict = {}
+for line in lines:
+    parts = line.split("\t")
 
+    currencyDict[parts[0]] = parts[1]
+
+amount = int(input("Enter the amount: "))
+print("Available options:\n")
+for item in currencyDict.keys():
+    print(item)
+
+target = input("Target currencry: ")
+rate = float(currencyDict.values)
+result = target*rate
+print(f"{amount} is equal to {result}")
